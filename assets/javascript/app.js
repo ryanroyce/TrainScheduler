09b8498f7cb3a2b0bdfb4688c8e1d91e94456ea8
dissonance <ryanminor01u@yahoo.com>
2018-11-16 14:44:30 -0600

    firebase linked as well as jQuery and bootstrap

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDRd7b3c5fbwSsD9s46vx-Tpyzp-q-bF_Y",
+    authDomain: "train-scheduler-e1fa5.firebaseapp.com",
+    databaseURL: "https://train-scheduler-e1fa5.firebaseio.com",
+    projectId: "train-scheduler-e1fa5",
+    storageBucket: "train-scheduler-e1fa5.appspot.com",
+    messagingSenderId: "908399834684"
+  };
+  firebase.initializeApp(config);
